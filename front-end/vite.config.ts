2d0650e6dee5d24f53ea758d4ef1bd63503be717
sunbagel <43621165+sunbagel@users.noreply.github.com>
2024-01-20 22:46:07 -0500

    package.json add

--- front-end/vite.config.ts
+++ front-end/vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
+import path from "path"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+ 
 export default defineConfig({
+  
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    port: 5173,
+    proxy: {
+    '/api': {
+        target: 'http://localhost:8080',
+        secure: false
+      }
+    },
+  },
 })
