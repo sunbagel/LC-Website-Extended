1d5eb5aaa84f6f4963813d8da5c01ea097e85a8c
sunbagel <43621165+sunbagel@users.noreply.github.com>
2024-01-29 23:04:29 -0500

    local.js file

---
+++ backend/strategies/local.js
@@ -0,0 +1,18 @@
+import { Strategy as LocalStrategy } from 'passport-local'
+import { passport } from 'passport'
+
+
+passport.use(new LocalStrategy(
+
+    async (username, password, done)=>{
+
+    }
+
+));
+
+
+
+
+
+
+
