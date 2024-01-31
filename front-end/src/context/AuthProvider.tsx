12eb9f7c84168cd9f506fa9371561241b2e47b2a
sunbagel <43621165+sunbagel@users.noreply.github.com>
2024-01-31 11:30:16 -0500

    add authprovider.tsx

---
+++ front-end/src/context/AuthProvider.tsx
@@ -0,0 +1,29 @@
+import { createContext, useState } from "react";
+
+const AuthContext = createContext({});
+
+// can import AuthProvider as a component to wrap children
+export const AuthProvider = ({children}) =>{
+
+    const [auth, setAuth] = useState({});
+
+    return (
+        <AuthContext.Provider value={{auth, setAuth}}>
+            {children}
+        </AuthContext.Provider>
+    )
+
+
+}
+
+// can also import the context itself for use
+export default AuthContext
+
+
+
+
+
+
+
+
+
