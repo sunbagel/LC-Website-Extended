e1663d8bd1d42bd6e331e839a52e909ad5a8ac04
sunbagel <43621165+sunbagel@users.noreply.github.com>
2024-02-01 20:50:02 -0500

    add redirect after login

--- front-end/src/pages/LoginPage.tsx
+++ front-end/src/pages/LoginPage.tsx
@@ -1,15 +1,31 @@
 import useAuth from "@/hooks/useAuth";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { Link, useNavigate, useLocation } from 'react-router-dom'
 
 
 
 const LoginPage = () => {
 
+  const { setAuth } = useAuth();
+
+  const navigate = useNavigate();
+  const location = useLocation();
+  // dynamically, where they came from
+  // in this case it'd really only be app-home
+  const from = location.state?.from?.pathname || '/app-home';
+
+
   const [user, setUser] = useState<string>('');
   const [pwd, setPwd] = useState<string>('');
   const [errMsg, setErrMsg] = useState<string>('');
 
-  const { setAuth } = useAuth();
+  // useEffect(()=>{
+
+  // })
+
+  useEffect(()=>{
+    setErrMsg('');
+  }, [user,pwd])
 
   const handleSubmit =  async () => {
     const userCredentials = {
@@ -33,6 +49,7 @@ const LoginPage = () => {
       // reset fields
       setUser('');
       setPwd('');
+      navigate(from, { replace: true });
     }).catch(err =>{
       if(!err?.response){
         setErrMsg('No Server Response');
